0c9291ed1cb7a9e0bcd4cddda46c5f90c0ad109f
Noal Brown <noalbrown1@gmail.com>
2020-07-22 16:06:50 -0500

    react-drills

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      filterString: '',
+      arr: ['Skyrim', 'Diablo', 'Fallout', 'World of Warships']
+    }
+  }
+
+  handleChange(filter) {
+    this.setState({ filterString: filter })
+  }
+
+  render() {
+    let newArr = this.state.arr
+      .filter((element, index) => {
+        return element.includes(this.state.filterString)
+      })
+      .map((element, index) => {
+        return <h1 key={index}>{element}</h1>
+      })
+
+    return (
+      <div className="App">
+        <input onChange={e => this.handleChange(e.target.value)} type="text" />
+        {newArr}
+      </div>
+    );
+  }
 }
 
 export default App;
